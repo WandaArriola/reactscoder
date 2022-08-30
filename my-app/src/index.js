3e633116c8cacc2f8963b131709b6cf3b479dd9e
WandaArriola <wanda.longo@hotmail.com>
2022-08-29 21:13:52 -0300

    component 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './component/App.jsx';
+import App from './component/NavBar.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App nombre="Wanda"/>

cc09719c019227799c49b117c5453c1e02d23f7a
WandaArriola <wanda.longo@hotmail.com>
2022-09-02 00:53:32 -0300

    modificación app.jsx

--- my-app/src/component/fragment/App.jsx
+++ my-app/src/component/fragment/App.jsx
@@ -25,36 +25,3 @@ export default App;
 
 
 
-//etso va arriba delreturn//
-
-/*const [cont, setCont ] = useState (0)
-
-function contador (operacion) {
-    if (operacion == "+"){+
-        setCont (cont +1)
-    } else {
-        setCont (cont -1)
-    }
-    console.log (cont)
-}*/
-
-
-
-
-//esto va dentro del return//
-
-/*<NavBar/>
-
-<p>
-    contador: {cont}
-</p>
-
-<button className='btn btn-secondary' onClick={() => contador("+")}>
-DISTE CLICK 
-</button>
-
-<button className='btn btn-primary'onClick={() => contador("-")} >
--DISTE CLICK 
-</button>  
-
-</>*/
